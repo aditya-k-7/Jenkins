e7e2f0b1cb12b6c52728e7291425666efa799c8a
aditya-k-7 <56600253+aditya-k-7@users.noreply.github.com>
2021-11-24 20:34:56 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -7,20 +7,13 @@ from Difference import difference
 class TestSum(unittest.TestCase):
     def test_list_int(self):
         
-        x=23, y=32
-        result = difference(x,y)
+        result = difference(23,32)
         self.assertEqual(result, 9)
-
-    def test_list_int(self):
         
-        x=2, y=3
-        result = difference(x,y)
+        result = difference(2,3)
         self.assertEqual(result, 1)
 
-    def test_list_int(self):
-       
-        x=100, y=5
-        result = difference(x,y)
+        result = difference(100,5)
         self.assertEqual(result, 95)
 
 if __name__ == '__main__':
