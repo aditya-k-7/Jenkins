83316ed7e8ef6be0d66dabaee3868aa19edf230f
aditya-k-7 <56600253+aditya-k-7@users.noreply.github.com>
2021-11-24 20:03:03 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -8,19 +8,19 @@ class TestSum(unittest.TestCase):
     def test_list_int(self):
         
         x=23, y=32
-        result = difference(data)
+        result = difference(x,y)
         self.assertEqual(result, 9)
 
     def test_list_int(self):
         
         x=2, y=3
-        result = difference(data)
+        result = difference(x,y)
         self.assertEqual(result, 1)
 
     def test_list_int(self):
        
         x=100, y=5
-        result = difference(data)
+        result = difference(x,y)
         self.assertEqual(result, 95)
 
 if __name__ == '__main__':
